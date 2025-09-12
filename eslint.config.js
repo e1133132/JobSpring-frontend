9b2aa4a626ea0292eba325c112a07e1de9df7e7c
e1133132 <e1133132@u.nus.edu>
2025-09-12 20:23:02 +0800

    lint SAST config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,18 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    rules: {
+      "react/react-in-jsx-scope": "off",  
+    },
+     settings: {
+      react: {
+        version: "detect", 
+      },
+    },
+    languageOptions: { globals: globals.browser } },
+    
   pluginReact.configs.flat.recommended,
 ]);
