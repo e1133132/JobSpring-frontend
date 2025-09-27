1a89c6228340eb2f8dc1eaeeedfb156acab7daaf
e1133132 <e1133132@u.nus.edu>
2025-09-27 23:08:55 +0800

    improve admin status function and add test file

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
+// @ts-nocheck
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+    process.env.CI_SKIP_LINT === '1' ? null : eslint({
+        exclude: ['**/*.test.*', '**/*.spec.*', '**/__tests__/**']
+    })
+    ],
     test: {
-        environment: 'jsdom',              
-        setupFiles: './src/setupTests.js',  
-        globals: true,                      
-        css: true,                         
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.js',
+        globals: true,
+        css: true,
         coverage: {
             reporter: ['text', 'html'],
             include: ['src/**/*.{js,jsx}'],
