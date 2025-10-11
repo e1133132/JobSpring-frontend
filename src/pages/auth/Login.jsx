dcf812d2aa8fc3d1f3577fa83802237d282e1eba
xxynlcwfp <xxxyoo1916@gmail.com>
2025-10-11 17:49:53 +0800

    updateJob UI/logic/navigation and fix hr checkIob bug

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -66,7 +66,7 @@ export default function Login() {
                     navigate("/home");
                     break;
                 case 1: // HR
-                    navigate("/home");
+                    navigate("/hr/JobPosition");
                     break;
                 case 2: // Admin
                     navigate("/admin/status");
