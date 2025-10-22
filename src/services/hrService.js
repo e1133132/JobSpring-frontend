63d997254b906a7d951f1a4d73acf9edc5ab355e
LittleLC <202003340112@zjut.edu.cn>
2025-10-22 18:06:23 +0800

    fix error prompt

--- src/services/hrService.js
+++ src/services/hrService.js
@@ -1,6 +1,6 @@
 import api from './api';
 
 export async function getCompanyId() {
-    const {data} = await api.get('/api/user/hr/company-id');
+    const {data} = await api.get('/api/hr/company-id');
     return data.companyId; 
 }
